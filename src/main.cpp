1f363b38b46e927d587e15ae1adee9792998f115
Christian KÃ¼hnel <christian.kuehnel@gmail.com>
2025-01-05 20:43:57 +0100

    a bit of tweaking of parameters
    just because.
     Also autoformater seemed to have modified something

--- src/main.cpp
+++ src/main.cpp
@@ -4,28 +4,26 @@
 #define X_RANDOM_RANGE 3
 #define Y_RANDOM_RANGE 3
 
-BleMouse bleMouse("DroChill Mouse Jiggler v1");
+BleMouse bleMouse("Logitech BlueM v1");
 
-int move_interval = 12; // with lower interval  notify(): << esp_ble_gatts_send_ notify: rc=-1 Unknown ESP_ERR error appears
+int move_interval = 12; // with lower interval  notify(): << esp_ble_gatts_send_
+                        // notify: rc=-1 Unknown ESP_ERR error appears
 int loop_interval = MINUTE * 1;
-uint16_t min_distance, max_distance;
+int min_distance, max_distance;
 
-void setup()
-{
-  min_distance = 30;
-  max_distance = 600;
+void setup() {
+  min_distance = 1;
+  max_distance = 3;
   bleMouse.begin();
 }
 
-void loop()
-{
+void loop() {
 
   int distance = random(min_distance, max_distance);
   int x = random(X_RANDOM_RANGE) - 1;
   int y = random(Y_RANDOM_RANGE) - 1;
 
-  for (int i = 0; i < distance; i++)
-  {
+  for (int i = 0; i < distance; i++) {
     bleMouse.move(x, y, 0);
     delay(move_interval);
   }
